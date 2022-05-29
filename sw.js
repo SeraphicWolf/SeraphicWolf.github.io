c8b7b1f18991ded013d56dd3c810b9b22f8b49d6
Seraphic <masterofpika11@gmail.com>
2022-05-29 15:29:44 -0400

    updates~

--- sw.js
+++ sw.js
@@ -32,8 +32,8 @@ workbox.precaching.precacheAndRoute([
   { url: '{{ post.url }}', revision: '{{ post.date | date: "%Y-%m-%d"}}' },
   {% endfor -%}
   { url: '/', revision: '{{ site.time | date: "%Y%m%d%H%M" }}' },
-  { url: '/page2', revision: '{{ site.time | date: "%Y%m%d%H%M" }}' },
-  { url: '/page3', revision: '{{ site.time | date: "%Y%m%d%H%M" }}' },
+  { url: '/aboutme', revision: '{{ site.time | date: "%Y%m%d%H%M" }}' },
+  { url: '/404', revision: '{{ site.time | date: "%Y%m%d%H%M" }}' },
   { url: '/assets/css/index.css', revision: '{{ site.time | date: "%Y%m%d%H%M" }}' }
 ])
 
