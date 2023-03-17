71e4799184984a0696cbd99651c0c99171fda434
SeraphicWolf <masterofpika11@gmail.com>
2023-03-16 21:04:17 -0400

    idk

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,1 @@
-navigator.serviceWorker && navigator.serviceWorker.register(‘./sw.js’).then(function(registration) {  console.log(‘Excellent, registered with scope: ‘, registration.scope);});
+navigator.serviceWorker && navigator.serviceWorker.register(‘./server-worker.js’).then(function(registration) {  console.log(‘Excellent, registered with scope: ‘, registration.scope);});
