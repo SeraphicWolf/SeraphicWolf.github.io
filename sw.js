c541b878ba1442b6206abf65596f74c6921b2ce3
SeraphicWolf <masterofpika11@gmail.com>
2023-03-16 23:11:51 -0400

    qwe

--- sw.js
+++ sw.js
@@ -53,7 +53,6 @@ registerRoute(
 if ("serviceWorker" in navigator) {
   // register service worker
   navigator.serviceWorker.register("sw.js");(
-  navigator.serviceWorker && navigator.serviceWorker.register((‘./sw.js’).then(function(registration) {  console.log(‘Excellent, registered with scope: ‘, registration.scope);});
   }
 );
 
